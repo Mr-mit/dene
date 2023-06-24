8dd1d5de7335cc2d9a44a92efd77c7453a638709
Mr mit <128430478+Mr-mit@users.noreply.github.com>
2023-06-25 01:19:09 +0200

    Spring data

---
+++ src/main/java/com/denek/dene/DeneApplication.java
@@ -0,0 +1,13 @@
+package com.denek.dene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denek/dene/DeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denek.dene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
